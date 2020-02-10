1ec2918fdbbed75b92a94f07f3f68ebb35cd875c
student <student@DESKTOP-CKU87KC>
2020-02-10 09:06:34 +0600

    sdgcjio0-ko,ll,l

---
+++ java/src/java/StudentTest.java
@@ -0,0 +1,18 @@
+package java;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		s1.setId(100);
+		s1.setName("pritha");
+		System.out.println(s1);
+		
+		Student s2= new Student();
+		s2.setId(150);
+		s2.setName("jui");
+		System.out.println(s2);
+		
+	}
+
+}
